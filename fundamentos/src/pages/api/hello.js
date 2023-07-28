1908080ba5fdea44cbe8c91bcb341eda4c15b16e
Nickz <nicolasaraujo845@gmail.com>
2023-07-28 11:24:56 -0300

    des

--- fundamentos/src/pages/api/hello.js
+++ fundamentos/src/pages/api/hello.js
@@ -1,5 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+    if(r3eq.method === "GET"){
+      handleGet(req,res)
+    }else{
+      res.status(405).send()
+  }
 }
+
+
+  function handleGet(req, res){
+    res.status(200).json({
+      id: 3,
+      nome: 'Maria',
+      email: 'mariamariamaria@xcfmail.com'
+    })
+  }
+  
